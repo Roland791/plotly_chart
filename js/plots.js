7b962b2ef8c8ddb3f30dff3c88f50d38f45ada98
Roland791 <84249213+Roland791@users.noreply.github.com>
2021-09-05 21:35:44 -0400

    Add files via upload

--- js/plots.js
+++ js/plots.js
@@ -2,7 +2,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   d3.json("samples.json").then((data) => {
-    console.log(data);
+   // console.log(data);
     var sampleNames = data.names;
     sampleNames.forEach((sample) => {
       selector
@@ -23,7 +23,7 @@ init();
     d3.json("samples.json").then((data) => {
       var metadata = data.metadata;
       var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-     // var result = resultArray[0];
+      var result = resultArray[0];
       var PANEL = d3.select("#sample-metadata");
   
       PANEL.html("");
