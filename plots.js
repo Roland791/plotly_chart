490071d6ddd73d9c5397b80fe28c3df3b7a05607
Roland791 <84249213+Roland791@users.noreply.github.com>
2021-09-05 21:15:42 -0400

    Add files via upload

--- plots.js
+++ plots.js
@@ -23,11 +23,11 @@ init();
     d3.json("samples.json").then((data) => {
       var metadata = data.metadata;
       var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-      var result = resultArray[0];
+     // var result = resultArray[0];
       var PANEL = d3.select("#sample-metadata");
   
       PANEL.html("");
-      Object.entries(result).forEach(([key, value]) => {
+      Object.entries(resultArray).forEach(([key, value]) => {
         PANEL.append("h6").text('${key.toUpperCase()}: ${value}');
       });
     //  PANEL.append("h6").text(result.location);
