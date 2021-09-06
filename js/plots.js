d378a5ed7a54d634da182cdfbe125ec859067c7a
Roland791 <84249213+Roland791@users.noreply.github.com>
2021-09-05 20:34:21 -0400

    Add files via upload

--- js/plots.js
+++ js/plots.js
@@ -1,9 +1,3 @@
-function init() {
-  data = [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }];
-  Plotly.newPlot("plot", data);
-};
 
 d3.selectAll("#dropdownMenu").on("change", updatePlotly);
 function updatePlotly() {
@@ -46,4 +40,3 @@ init();
     });
   }
 
-  
